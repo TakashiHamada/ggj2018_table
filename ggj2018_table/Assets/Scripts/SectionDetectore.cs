8fc983e0ceafe3c430bd7b68b5adba379e2e3749
Takashi Hamada <hamada@gift10.net>
2018-01-27 20:37:57 +0900

    角度検知機能の作成

---
+++ ggj2018_table/Assets/Scripts/SectionDetectore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SectionDetectore : MonoBehaviour {
+	[SerializeField] int _division = 8;
+	[SerializeField] int _range = 5;
+	[SerializeField] TableInput _table_input;
+	private int _interval;
+	private int _current_id = -1;
+	int GetCurrentId () {
+		return _current_id;
+	}
+	void Start () {
+		_interval = 360 / _division;	
+	}
+	void Update () {
+		var euler = _table_input.GetEuler();
+		for(int id = 0; id < _division; id++) {
+			int min = (_interval * id) - _range;
+			if((_interval * id) - _range < euler && euler < (_interval * id) + _range) {
+				_current_id = id;
+				
+			} else
+			// 対策
+			if(euler > 360 - _range) _current_id = 0;	
+		}
+	}
+}
