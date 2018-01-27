f2e11d6b2ea6f843d6dd1a8cbddbc50f048d9136
Takashi Hamada <hamada@gift10.net>
2018-01-27 19:38:47 +0900

    input のラッパを作成

--- ggj2018_table/Assets/Scripts/TableInput.cs
+++ ggj2018_table/Assets/Scripts/TableInput.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TableInput : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+	[SerializeField] bool _is_test_mode = false;
+	private float _euler = 0f;
+	private bool[] _buttons = new bool[3];
+	public int GetEuler () {
+		return (int)_euler;
 	}
-	
-	// Update is called once per frame
+	public bool GetButton(int num) {
+		return _buttons[num];
+	}
 	void Update () {
-		
+		Input.gyro.enabled = true;
+		_euler = Input.gyro.attitude.eulerAngles.z;
+		_buttons[0] = Input.GetKey(KeyCode.Joystick1Button0);
+		_buttons[1] = Input.GetKey(KeyCode.Joystick1Button1);
+		_buttons[2] = Input.GetKey(KeyCode.Joystick1Button2);
 	}
 }
