c161b4fde6a6ad14f5e77dc88123d74c2ff5d627
waka2ya <waka.2ya@gmail.com>
2018-01-28 12:25:46 +0900

    リザルト画面から5秒後にタイトルへ遷移

--- ggj2018_table/Assets/Scripts/ResultManager.cs
+++ ggj2018_table/Assets/Scripts/ResultManager.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ResultManager : MonoBehaviour {
     [SerializeField]
     Text _score_text;
 
+    [SerializeField]
+    float timer = 10.0f;
 
     // Use this for initialization
     void Start () {
@@ -17,6 +20,10 @@ public class ResultManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        timer -= Time.deltaTime;
+        if(timer < 0.0f)
+        {
+            SceneManager.LoadScene("Title");
+        }
+    }
 }
