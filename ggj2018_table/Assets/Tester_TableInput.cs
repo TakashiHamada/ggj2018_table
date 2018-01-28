83920a1c1df48fa74c590f7f1d7768aa4ea852ff
takashiski <takashiskibb@gmail.com>
2018-01-28 11:53:09 +0900

    入力の単体テストシーン

---
+++ ggj2018_table/Assets/Tester_TableInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester_TableInput : MonoBehaviour {
+    TableInput inputManager;
+	// Use this for initialization
+	void Start () {
+        inputManager = this.GetComponent<TableInput>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("euler:"+inputManager.GetEuler());
+        for (int i = 0; i < 4; i += 1)
+        {
+            if (inputManager.GetButton(i))
+                Debug.Log("button " + i + "pressed");
+        }
+	}
+}
